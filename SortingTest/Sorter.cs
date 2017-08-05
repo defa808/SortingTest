bc6189b1ecaa169911721378ba6a866f5043119e
Alexander <alex.gavriliak@gmail.com>
2017-08-05 19:08:32 +0300

    Implemented method Sort

--- SortingTest/Sorter.cs
+++ SortingTest/Sorter.cs
@@ -12,7 +12,19 @@ namespace SortingTest
     {
         public IEnumerable<T> Sort(T[] array)
         {
-            throw new InvalidOperationException();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j].CompareTo(array[j+1]) > 0)
+                    {
+                        var tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                }
+            }
+            return array;
         }
     }
 }
